3ae1e1dc8538dd1842641f548b24245ada7b6313
Renat Mansurov <renmanz@yandex.ru>
2022-02-27 01:41:38 +0300

    Draw grid and array

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
-# import random
+# NOTES
+# U can use more flags than there are mines
+
 import pygame
+import numpy as np
+from random import randrange
 from pygame.locals import Rect, QUIT
 
 
-# globals
 MINE_SIZE = 16
 SETTINGS = {
     'begginer':     {'size':  9, 'mines': 10, 'screen': (164, 206)},
@@ -23,7 +26,6 @@ class Seemine:
         self.flags = SETTINGS[difficulty]['mines']
         self.screen_size = SETTINGS[difficulty]['screen']
         self.screen = pygame.display.set_mode(self.screen_size)
-        # borders are permanent
         self.borders = (
             ((0, self.screen_size[1]-1),
              (self.screen_size[0], self.screen_size[1]-1)),
@@ -41,17 +43,29 @@ class Seemine:
              (8, 42)),
             ((self.screen_size[0]-9, 8),
              (self.screen_size[0]-9, 42)),
-            ((0, 0), (0, 0)),  # dark
-            ((0, 0), (0, 0)),  # white
-            ((0, 0), (0, 0)),  # dark
-            ((0, 0), (0, 0))   # white
+            ((8, 50),
+             (8, self.screen_size[1]-9)),
+            ((self.screen_size[0]-9, 50),
+             (self.screen_size[0]-9, self.screen_size[1]-9)),
+            ((8, 50),
+             (self.screen_size[0]-9, 50)),
+            ((8, self.screen_size[1]-9),
+             (self.screen_size[0]-9, self.screen_size[1]-9))
         )
 
-    def create_field():
-        pass
+    def create_field(self):
+        self.field = np.array(
+            [[0 for _ in range(self.size)] for _ in range(self.size)])
+        print(self.field)  # TODO: Remove
 
-    def set_mines():
-        pass
+    def set_mines(self):
+        count = 0
+        while count < self.mines:
+            x, y = randrange(0, self.size), randrange(0, self.size)
+            if self.field[y][x] != -1:
+                self.field[y][x] = -1
+                count += 1
+        print(self.field)  # TODO: Remove
 
     def set_values():
         pass
@@ -59,17 +73,37 @@ class Seemine:
     def neighbours():
         pass
 
-    def draw_screen(self):
+    def change_difficulty():
         pass
 
-    def draw_border(self):
+    def draw_borders(self):
         for i, border in enumerate(self.borders):
             color = COLORS['dark'] if i % 2 == 0 else 'white'
             pygame.draw.line(self.screen, pygame.Color(color),
                              border[0], border[1], width=2)
 
-    def draw_field():
+    def draw_grid(self):
+        for i in range(self.size):
+            pygame.draw.line(self.screen, pygame.Color(COLORS['dark']),
+                             (10+(16*i), 50),
+                             (10+(16*i), self.screen_size[1]-10), width=1)
+            pygame.draw.line(self.screen, pygame.Color(COLORS['dark']),
+                             (8, 52+(16*i)),
+                             (self.screen_size[0]-10, 52+(16*i)), width=1)
+
+    # depends on current state of field
+    def draw_field_overlay():
         pass
+
+    def draw_counter():
+        pass
+
+    def draw_dropdown_menu():
+        pass
+
+    def prep(self):
+        self.create_field()
+        self.set_mines()
 
     # TODO: refactoring of this spaghetti
     def run(self):
@@ -77,16 +111,9 @@ class Seemine:
         pygame.display.set_caption('Seemine')
         self.screen.fill(pygame.Color(COLORS['gray']))
 
-        # TODO: Remove
-        temp_margin = 8
-        temp_size = MINE_SIZE * self.size + 4
-        # game rect
-        pygame.draw.rect(self.screen, pygame.Color('black'),
-                         Rect(temp_margin, temp_margin+42, temp_size,
-                              temp_size))
-
-        self.draw_screen()
-        self.draw_border()
+        self.prep()
+        self.draw_borders()  # should be last step
+        self.draw_grid()
 
         while True:
             pygame.display.flip()
@@ -97,6 +124,7 @@ class Seemine:
 
 if __name__ == '__main__':
     # seemine = Seemine()
-    # seemine = Seemine('intermediate')
-    seemine = Seemine('expert')
+    seemine = Seemine('intermediate')
+    # seemine = Seemine('expert')
+
     seemine.run()
